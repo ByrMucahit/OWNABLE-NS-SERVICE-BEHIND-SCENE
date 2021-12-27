6b63ea517d300799da73533730850fc79d70b20b
ByrMucahit <m.mucahitbayar@gmail.com>
2021-12-27 23:15:51 +0300

    Spring Initialzt
    Spring framework has been added into project. Dimensions has been built. Dimesions're api, business, core, data access and entities.

---
+++ nftService/src/main/java/ownable/nftService/NftServiceApplication.java
@@ -0,0 +1,13 @@
+package ownable.nftService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftServiceApplication.class, args);
+	}
+
+}
---
+++ nftService/src/test/java/ownable/nftService/NftServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ownable.nftService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
